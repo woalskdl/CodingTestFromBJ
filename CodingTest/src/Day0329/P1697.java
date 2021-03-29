53119558b9307491335afce4e25500694e81ce7b
wooljam <woalskdl@gmail.com>
2021-03-30 07:49:39 +0900

    0330 Commit

---
+++ CodingTest/src/Day0329/P1697.java
@@ -0,0 +1,67 @@
+package Day0329;
+
+import java.util.*;
+
+class HAS{
+	int[] check = new int[100001];
+	boolean[] c = new boolean[100001];
+	int pA;
+	int pB;
+	
+	HAS(int a, int b) {
+		this.pA = a;
+		this.pB = b;
+		c[a] = true;
+	}
+	
+	int run() {
+		Queue<Integer> track = new LinkedList<>();
+		
+		while(pA != pB) {
+			
+			if(pA + 1 < c.length) {
+				if(!c[pA + 1]) {
+					track.offer(pA + 1);
+					check[pA + 1] = check[pA] + 1;
+					c[pA + 1] = true;
+				}
+			}
+			
+			if(pA - 1 >= 0) {
+				if(!c[pA - 1]) {
+					track.offer(pA - 1);
+					check[pA - 1] = check[pA] + 1;
+					c[pA - 1] = true;
+				}
+			}
+			
+			if(pA * 2 < c.length) {
+				if(!c[pA * 2]) {
+					track.offer(pA * 2);
+					check[pA * 2] = check[pA] + 1;
+					c[pA * 2] = true;
+				}
+			}
+			
+			pA = track.poll();
+		}
+		
+		return check[pA];
+	}
+	
+}
+
+public class P1697 {
+	public static void main(String[] args) {
+		
+	Scanner scan = new Scanner(System.in);
+	
+	int N = scan.nextInt();
+	int K = scan.nextInt();
+	
+	HAS h = new HAS(N,K);
+	System.out.println(h.run());
+	
+		
+	}
+}
---
+++ CodingTest/src/Day0329/test.java
@@ -0,0 +1,14 @@
+package Day0329;
+
+public class test {
+	public static void main(String[] args) {
+		
+		int x = 4;
+		int y = 5;
+		
+		if(!(x == 4 && y == 5)) {
+			System.out.println("dddd");
+		}
+		
+	}
+}
