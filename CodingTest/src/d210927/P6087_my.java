13fdeaa3cd0bb58e68ccb3d4a147dc33eb4f6a85
jay <jay.wekeep@gmail.com>
2021-09-27 09:28:43 +0900

    6087 잘푼거같은데 통과 안된다. 뭐가 문제일까

---
+++ CodingTest/src/d210927/P6087_my.java
@@ -0,0 +1,94 @@
+package d210927;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P6087_my {
+    static private int W;
+    static private int H;
+    static private char[][] map;
+    static private int[][] visited;
+    static private List<Node> nodes;
+
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    static private List<Integer> countList;
+    static private class Node{
+        int y;
+        int x;
+        int dir;
+        int dirCount;
+
+        public Node(int y, int x, int dir) {
+            this.y = y;
+            this.x = x;
+            this.dir = dir;
+        }
+
+        public Node(int y, int x, int dir, int dirCount) {
+            this.y = y;
+            this.x = x;
+            this.dir = dir;
+            this.dirCount = dirCount;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        nodes = new ArrayList<>();
+
+        map = new char[H][W];
+        visited = new int[H][W];
+        for(int i=0; i<H; i++){
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
+            for(int j=0; j<W; j++){
+                map[i][j] = line.charAt(j);
+                visited[i][j] = -1;
+                if(map[i][j] == 'C')
+                    nodes.add(new Node(i, j, -1));
+            }
+        }
+
+        System.out.println(bfs());
+
+    }
+
+    static private int bfs(){
+        Queue<Node> queue = new LinkedList<>();
+        Node start = nodes.get(0);
+        queue.add(start);
+        visited[start.y][start.x] = 0;
+
+        while (!queue.isEmpty()){
+            Node node = queue.poll();
+
+            for(int i=0; i<4; i++){
+                int ny = node.y + dy[i];
+                int nx = node.x + dx[i];
+
+                if(inArea(ny, nx) && (visited[ny][nx] == -1 || visited[ny][nx] > node.dirCount)){
+                    int dirCount = visited[node.y][node.x];
+                    if(node.dir != -1 && node.dir != i)
+                        dirCount += 1;
+                    queue.add(new Node(ny, nx, i, dirCount));
+                    visited[ny][nx] = dirCount;
+                }
+            }
+        }
+
+        return visited[nodes.get(1).y][nodes.get(1).x];
+    }
+
+    static private boolean inArea(int y, int x){
+        return y >= 0 && x >= 0 && y < H && x < W && map[y][x] != '*';
+    }
+}
