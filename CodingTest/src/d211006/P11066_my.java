93613390b9085fc2276d702975e6aadba8970e8e
jay <jay.wekeep@gmail.com>
2021-10-06 09:39:11 +0900

    11066 뭐가문제. 아직 미해결

---
+++ CodingTest/src/d211006/P11066_my.java
@@ -0,0 +1,122 @@
+package d211006;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P11066_my {
+
+    private static int T;
+    private static List<Integer> sumList;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        T = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<T; i++){
+            st = new StringTokenizer(br.readLine());
+            sumList = new ArrayList<>();
+
+            int K = Integer.parseInt(st.nextToken());
+            int[] files = new int[K];
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j=0; j<K; j++)
+                files[j] = Integer.parseInt(st.nextToken());
+
+            System.out.println(bfs(new Plus(0, files)));
+//            dp(0, files);
+//            System.out.println(sumList.toString());
+//            System.out.println(Collections.min(sumList));
+        }
+    }
+
+    static class Plus {
+        int sum;
+        int[] files;
+
+        public Plus(int sum, int[] files) {
+            this.sum = sum;
+            this.files = files;
+        }
+    }
+
+    private static int bfs(Plus plus){
+        Queue<Plus> queue = new LinkedList<>();
+        queue.add(plus);
+
+        while (!queue.isEmpty()){
+            Plus p = queue.poll();
+
+            int[] files = p.files;
+            int sum = p.sum;
+
+            if(files.length == 1){
+                sumList.add(sum);
+                System.out.println("sum : " + sum);
+                continue;
+            }
+
+            for(int i=0, length = files.length; i<length - 1; i++){
+                int[] temp = new int[length - 1];
+                int num = sum;
+
+                System.out.println("i : " + i);
+
+                for(int j=0, tempLength = temp.length; j<tempLength; j++){
+                    if(j == i){
+                        temp[j] = files[j] + files[j + 1];
+                        num += temp[j];
+                        System.out.print(temp[j] + " ");
+                    }else if(j > i){
+                        temp[j] = files[j + 1];
+                        System.out.print(temp[j] + " ");
+                    }else{
+                        temp[j] = files[j];
+                        System.out.print(temp[j] + " ");
+                    }
+                }
+
+                System.out.println();
+                queue.add(new Plus(num, temp));
+            }
+        }
+
+        return Collections.min(sumList);
+    }
+
+    private static int dp(int sum, int[] files){
+        if(files.length == 1){
+            sumList.add(sum);
+            System.out.println("sum : " + sum);
+            return sum;
+        }
+
+        for(int i=0, length = files.length; i<length-1; i++){
+            int[] temp = new int[length - 1];
+            int num = sum;
+
+            for(int j=0, tempLength = temp.length; j<tempLength; j++){
+                if(j == i){
+                    temp[j] = files[j] + files[j + 1];
+                    num += temp[j];
+                    System.out.print(temp[j] + " ");
+                }else if(j > i){
+                    temp[j] = files[j + 1];
+                    System.out.print(temp[j] + " ");
+                }else{
+                    temp[j] = files[j];
+                    System.out.print(temp[j] + " ");
+                }
+            }
+            System.out.println();
+
+            sumList.add(dp(num, temp));
+        }
+
+        return 0;
+    }
+}
+
