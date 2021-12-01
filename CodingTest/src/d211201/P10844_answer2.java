3857376ba4a9fac506ebc7fa1eaffa35f171678d
jay <jay.wekeep@gmail.com>
2021-12-01 09:13:45 +0900

    10844 ...알고리즘 하는 사람들은 천재인가부다. 나도 천재가 되어야지.

---
+++ CodingTest/src/d211201/P10844_answer2.java
@@ -0,0 +1,39 @@
+package d211201;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P10844_answer2 {
+    private final static long MOD = 1000000000;
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[N + 1][10];
+
+        for(int i=1; i<10; i++)
+            dp[1][i] = 1;
+
+        for(int i=2; i<=N; i++){
+            for(int j=0; j<10; j++){
+                if(j == 0)
+                    dp[i][0] = dp[i - 1][1] % MOD;
+                else if(j == 9)
+                    dp[i][9] = dp[i - 1][8] % MOD;
+                else
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+            }
+        }
+
+        long result = 0;
+
+        for(int i=0; i<10; i++)
+            result += dp[N][i];
+
+        System.out.println(result % MOD);
+    }
+}
