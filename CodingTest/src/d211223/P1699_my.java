a3ed996b0e4a2113c83d6dce029bed7e60cb4147
jay <jay.wekeep@gmail.com>
2021-12-23 09:20:03 +0900

    1600 반례 존재. 142 >> (정답) 3 / (내답) 4

---
+++ CodingTest/src/d211223/P1699_my.java
@@ -0,0 +1,33 @@
+package d211223;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1699_my {
+    private static int N;
+    private static Integer[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new Integer[N + 1];
+
+        System.out.println(dp(N, 0));
+    }
+
+    private static int dp(int val, int count){
+        if(val == 0)
+            return count;
+
+        if(arr[val] != null)
+            return arr[val];
+
+        int a = (int) Math.floor(Math.sqrt(val));
+        arr[val] = Integer.MAX_VALUE;
+        for(int i=a; i>0; i--)
+            arr[val] = Math.min(arr[val], dp(val - (int) Math.pow(i, 2), count + 1));
+
+        return arr[val];
+    }
+}
