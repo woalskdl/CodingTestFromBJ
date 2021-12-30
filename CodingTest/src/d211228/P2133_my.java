e425267c34987b7380225d8260133dc0ccd0ac35
jay <jay.wekeep@gmail.com>
2021-12-30 09:15:52 +0900

    2133 ㅠㅠ아닌가

--- CodingTest/src/d211228/P2133_my.java
+++ CodingTest/src/d211228/P2133_my.java
@@ -17,12 +17,32 @@ public class P2133_my {
             return;
         }
 
-        arr = new Integer[N + 1][3];
+        arr = new Integer[N + 1][10];
 
-        // 0 > 첫번쨰 1, 1 > 두번째 1, 2 > 2
-        arr[1][0] = 2;
-        arr[1][1] = 0;
-        arr[1][2] = 2;
+//        220 > 0
+//        022 > 1
+//        000 > 2
+//        221 > 3 //
+//        001 > 4
+//        122 > 5 //
+//        100 > 6
+//        111 > 7 //
+//        110 > 8
+//        011 > 9
+
+        arr[1][0] = arr[1][1] = arr[1][2] = 1;
+        arr[1][3] = arr[1][4] = arr[1][5] = arr[1][6] = arr[1][7] = arr[1][8] = arr[1][9] = 0;
+        
+        for(int i=2; i<=N; i++){
+            arr[i][0] = arr[i][1] = arr[i][2] = arr[i - 1][3] + arr[i - 1][5] + arr[i - 1][7];
+            arr[i][3] = arr[i][4] = arr[i - 1][0];
+            arr[i][5] = arr[i][6] = arr[i - 1][1];
+            arr[i][7] = arr[i - 1][2];
+            arr[i][8] = arr[i - 1][4];
+            arr[i][9] = arr[i - 1][6];
+        }
+
+        System.out.println(arr[N][3] + arr[N][5] + arr[N][7]);
 
     }
 
@@ -40,22 +60,39 @@ public class P2133_my {
         return 0;
     }
 
-    private int recur(int pos, int[][] input, int count){
+    private int recur(int pos, int idx, int count){
         if(pos == 1)
-            return count + input[pos][0] + input[pos][1] + input[pos][2];
+            return count;
 
-        int[][] arr = new int[3][3];
+        arr[pos][0] = arr[pos][1] = arr[pos][2] = recur(pos - 1, 3, arr[pos - 1][3]) + recur(pos - 1, 5, arr[pos - 1][5]) + recur(pos - 1, 7, arr[pos - 1][7]);
+        arr[pos][3] = arr[pos][4] = recur(pos - 1, 0, arr[pos - 1][0]);
+        arr[pos][5] = arr[pos][6] = recur(pos - 1, 1, arr[pos - 1][1]);
+        arr[pos][7] = recur(pos - 1, 2, arr[pos - 1][2]);
+        arr[pos][8] = recur(pos - 1, 4, arr[pos - 1][4]);
+        arr[pos][9] = recur(pos - 1, 6, arr[pos - 1][6]);
 
-        for(int i=0; i<3; i++){
-            arr[i][0] = input[i][1] + input[i][2];
-            arr[i][1] = input[i][0];
-            arr[i][2] = input[i][1] + input[i][2];
+        for(int i : arr[pos])
+            count += arr[pos][i];
 
-            count += arr[i][0];
-            count += arr[i][1];
-            count += arr[i][2];
-        }
-
-        return recur(pos - 1, arr, count);
+        return count;
     }
+
+//    private int recur(int pos, int[][] input, int count){
+//        if(pos == 1)
+//            return count + input[pos][0] + input[pos][1] + input[pos][2];
+//
+//        int[][] arr = new int[3][3];
+//
+//        for(int i=0; i<3; i++){
+//            arr[i][0] = input[i][1] + input[i][2];
+//            arr[i][1] = input[i][0];
+//            arr[i][2] = input[i][1] + input[i][2];
+//
+//            count += arr[i][0];
+//            count += arr[i][1];
+//            count += arr[i][2];
+//        }
+//
+//        return recur(pos - 1, arr, count);
+//    }
 }
